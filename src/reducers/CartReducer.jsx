67483b114c81118ea9dcd5e12794d6aaeb039cd8
alchemistbg <alchemist_bg@abv.bg>
2020-03-02 00:23:49 +0200

    Add cart reducers

---
+++ src/reducers/CartReducer.jsx
@@ -0,0 +1,82 @@
+export const initialCartState = {
+    cart: []
+};
+
+export const cartReducer = (state, action) => {
+    let itemIndex;
+    if (action.item) {
+        itemIndex = state.cart.findIndex(item => item.id === action.item.id);
+    }
+
+    const clearCart = () => {
+        localStorage.removeItem("myCart");
+    }
+
+    const updateCart = (data) => {
+        localStorage.setItem("myCart", JSON.stringify(data));
+    }
+
+    switch (action.type) {
+        case 'LOAD_DATA_FROM_STORAGE':
+            state.cart = JSON.parse(action.item);
+            return {
+                cart: state.cart
+            }
+
+        case 'ADD_TO_CART':
+            if (itemIndex > -1) {
+                state.cart[itemIndex].productSize = action.productSize;
+                state.cart[itemIndex].productQty += action.productQty;
+                state.cart[itemIndex].totalPrice = (state.cart[itemIndex].productQty * state.cart[itemIndex].price).toFixed(2);
+                updateCart(state.cart);
+                return {
+                    cart: state.cart
+                }
+            } else {
+                action.item.productSize = action.productSize;
+                action.item.productQty = action.productQty;
+                action.item.totalPrice = (action.item.productQty * action.item.price).toFixed(2);
+                state.cart.push(action.item);
+                updateCart(state.cart);
+                return {
+                    cart: state.cart
+                }
+            }
+
+        case 'INCREMENT':
+            state.cart[itemIndex].productQty += 1
+            state.cart[itemIndex].totalPrice = (state.cart[itemIndex].productQty * state.cart[itemIndex].price).toFixed(2);
+            updateCart(state.cart);
+            return {
+                cart: state.cart
+            }
+
+        case 'DECREMENT':
+            if (state.cart[itemIndex].productQty > 1) {
+                state.cart[itemIndex].productQty -= 1;
+                state.cart[itemIndex].totalPrice = (state.cart[itemIndex].productQty * state.cart[itemIndex].price).toFixed(2);
+            }
+            updateCart(state.cart);
+            return {
+                cart: state.cart
+            }
+
+        case 'REMOVE_FROM_CART':
+            if (state.cart.length === 1) {
+                clearCart();
+            } else {
+                state.cart = state.cart.filter((item) => item.id !== action.item.id);
+                updateCart(state.cart);
+            }
+            return {
+                cart: state.cart.filter((item) => item.id !== action.item.id)
+            }
+
+        case 'CHECKOUT':
+            clearCart();
+            return { cart: [] };
+
+        default:
+            return state;
+    }
+}
