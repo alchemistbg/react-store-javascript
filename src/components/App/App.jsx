c119cfc4b17c62b88f2cae9ce966687f134100f5
alchemistbg <alchemist_bg@abv.bg>
2020-03-05 17:14:20 +0200

    Update component
    - Add document title
    - All paths are formatted the same way

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,22 +4,21 @@ import './App.scss';
 import React, { useReducer } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+import Header from './../Header/Header';
+import Footer from './../Footer/Footer';
 
 import ProductList from './../ProductList/ProductList';
 import ProductDetails from './../ProductDetails/ProductDetails';
 import Cart from './../Cart/Cart';
 
-import { CartProvider } from '../../context/CartContext';
-import { cartReducer, initialCartState } from '../../reducers/CartReducer';
+import { CartProvider } from './../../context/CartContext';
+import { cartReducer, initialCartState } from './../../reducers/CartReducer';
 
 function App() {
 
 	const useCartState = useReducer(cartReducer, initialCartState);
 
 	return (
-		document.title = "Compass Store",
 		<div className="site-container">
 			<Router>
 				<CartProvider value={useCartState}>
