679966b32ec7a0074a47e12cc4dfca066567b3fb
alchemistbg <alchemist_bg@abv.bg>
2020-03-06 16:10:55 +0200

    Replace part of the logo text

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -6,7 +6,7 @@ function Logo() {
             <img className="logo-img" src={"/static/images/logo.png"} alt="" />
             <div className="logo-wrapper">
                 <h2 className="logo-text">Compass Shop</h2>
-                <h6 className="logo-subtext">for true fans</h6>
+                <h6 className="logo-subtext">for DEDICATED fans</h6>
             </div>
         </div>
     )
